bed965f3051e96f00b33773e1b2f0f25500375e0
Jim McAndrew <jim@dronelink.com>
2023-01-19 12:16:49 -0700

    Updated DroneSession.resetPayloads

--- DronelinkAutel/AutelDroneSession.swift
+++ DronelinkAutel/AutelDroneSession.swift
@@ -846,8 +846,17 @@ extension AutelDroneSession: DroneSession {
     public func batteryState(index: UInt) -> DronelinkCore.DatedValue<DronelinkCore.BatteryStateAdapter>? { nil }
     
     public func resetPayloads() {
-        sendResetGimbalCommands()
-        sendResetCameraCommands()
+        resetPayloads(gimbal: true, camera: true)
+    }
+    
+    public func resetPayloads(gimbal: Bool, camera: Bool) {
+        if gimbal {
+            sendResetGimbalCommands()
+        }
+        
+        if camera {
+            sendResetCameraCommands()
+        }
     }
     
     public func close() {
